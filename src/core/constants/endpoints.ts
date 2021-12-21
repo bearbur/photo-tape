34011f3b9da07f3c2bb4a11564876b12132c7063
Victor Naumov <bearbur@gmail.com>
2021-12-21 20:31:32 +0300

    endpoint fix from NODE_ENV

--- src/core/constants/endpoints.ts
+++ src/core/constants/endpoints.ts
@@ -1,8 +1,12 @@
 const API_HOST = 'http://localhost';
-const API_PORT = '80';
+const API_PORT_DEVELOPMENT = '8080';
+const API_PORT_PRODUCTION = '80';
 const API_PREFIX = 'photo_tape_api';
 
-const combineBaseUrl = (apiUrl: string): string => `${API_HOST}:${API_PORT}/${API_PREFIX}/${apiUrl}`;
+const combineBaseUrl = (apiUrl: string): string => {
+    const port = process.env.NODE_ENV.toLowerCase() === 'development' ? API_PORT_DEVELOPMENT : API_PORT_PRODUCTION;
+    return `${API_HOST}:${port}/${API_PREFIX}/${apiUrl}`;
+};
 
 const userPrefix = (partUrl: string) => `user/${partUrl}`;
 
@@ -16,11 +20,11 @@ const USER_LOGOUT = combineBaseUrl(userPrefix(LOGOUT));
 const USER_PROFILE = combineBaseUrl(userPrefix(PROFILE));
 const USER_CHANGE_PASS = combineBaseUrl(userPrefix(CHANGE_PASS));
 
- const endpoints : {[key: string]: string} = {
-     login: USER_LOGIN,
-     logout: USER_LOGOUT,
-     profile: USER_PROFILE,
-     changePass: USER_CHANGE_PASS
-}
+const endpoints: { [key: string]: string } = {
+    login: USER_LOGIN,
+    logout: USER_LOGOUT,
+    profile: USER_PROFILE,
+    changePass: USER_CHANGE_PASS,
+};
 
 export default endpoints;
